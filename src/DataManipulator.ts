2533cd663037a10f3a90c940afd20dd5e3f6420f
Rishabh Agarwal <rishabh17082004@gmail.com>
2024-05-07 10:49:27 +0530

    Modified existing graph to show lower bounds and upper bounds to trigger alert

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
+import { timeStamp } from 'console';
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  lower_bound: number,
+  upper_bound: number,
+  trigger_alert: number | undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const price_ABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const price_DEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+    const ratio = price_ABC/price_DEF;
+    const upperbound = 1 + 0.05;
+    const lowerbound = 1 - 0.05;
+    return {
+      price_abc: price_ABC,
+      price_def: price_DEF,
+      ratio,
+      lower_bound: lowerbound,
+      upper_bound: upperbound,
+      trigger_alert: (ratio>upperbound|| ratio<lowerbound)?ratio:undefined,
+      timestamp: serverResponds[0].timestamp>serverResponds[1].timestamp?serverResponds[0].timestamp:serverResponds[1].timestamp,
+    }
   }
 }
